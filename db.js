11f7b2582ab1bc3f963fca086c7e09403e07de68
drwopadi_2002 <drowpadi.rajamohanan@ibilglobal.com>
2024-12-18 15:47:29 +0530

    manju task added

--- db.js
+++ db.js
@@ -7,9 +7,7 @@ let connectDb = async () => {
   if (!db) {
     try {
       console.log("connected to mongo db");
-      client = new MongoClient.connect("mongodb://localhost:27017", {
-        useUnifiedTopology: true,
-      });
+      client = new MongoClient.connect("mongodb://localhost:27017");
       console.log(client);
       db = client.db("task");
       console.log(db);
